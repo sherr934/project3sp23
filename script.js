065ac9f9ae6535b47df085fb54e9f43aa49c82a2
sherr934 <sherr934@newschool.edu>
2023-05-02 09:15:06 -0400

    kuk sound

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let parentSect = document.querySelector("#parent")
+let kuk = document.querySelector("#kuk")
 let quaa = document.querySelector("#quaa")
+let moan = document.querySelector("#moan")
+
 let squirrelDOM = "<div>";
 const squirrelData = document.getElementById('parent');
-const quaas = [
+// const kuks = [
 
-  ]
+//   ]
 
 async function logJSONData() {
   const response = await fetch("https://data.cityofnewyork.us/resource/vfnx-vebw.json");
@@ -12,43 +15,114 @@ async function logJSONData() {
   console.log(jsonData);
 
   for(let i=0; i<jsonData.length; i++){
+    //cinnamon fur
     if(jsonData[i].primary_fur_color==="Cinnamon"){
       let div = document.createElement("div");
+      if(jsonData[i].kuks===true){
+        div.addEventListener("click", ()=>{
+          kuk.play();
+        })
+        //only ones with kuks show
+        div.classList.add("cinnamon");
+        parentSect.append(div)
+      }
+
       if(jsonData[i].quaas===true){
-      quaas.push (div);
-    }
-      div.classList.add("cinnamon");
-      parentSect.append(div)
+        div.addEventListener("click", ()=>{
+          quaa.play();
+        })
+        //only ones with kuks show
+        div.classList.add("cinnamon");
+        parentSect.append(div)
+      }
+      
+      if(jsonData[i].moans===true){
+        div.addEventListener("click", ()=>{
+          moan.play();
+        })
+        //only ones with kuks show
+        div.classList.add("cinnamon");
+        parentSect.append(div)
+      }
+
     };
+
+    //gray fur
     if(jsonData[i].primary_fur_color==="Gray"){
       let div = document.createElement("div");
-      if(jsonData[i].quaas===true){
-      quaas.push (div);
-    }
+      if(jsonData[i].kuks===true){
+        div.addEventListener("click", ()=>{
+          kuk.play();
+        })
+      //only ones with kuks show
       div.classList.add("gray");
-      parentSect.append(div)
+      parentSect.append(div) 
+      }
+
+      if(jsonData[i].quaas===true){
+        div.addEventListener("click", ()=>{
+          quaa.play();
+        })
+      //only ones with kuks show
+      div.classList.add("gray");
+      parentSect.append(div) 
+      }
+
+      if(jsonData[i].moans===true){
+        div.addEventListener("click", ()=>{
+          moan.play();
+        })
+      //only ones with kuks show
+      div.classList.add("gray");
+      parentSect.append(div) 
+      }
+ 
     };
+
+    //black fur
     if(jsonData[i].primary_fur_color==="Black"){
       let div = document.createElement("div");
+      if(jsonData[i].kuks===true){
+        div.addEventListener("click", ()=>{
+          kuk.play();
+        })
+        //only ones with kuks show
+        div.classList.add("black");
+        parentSect.append(div)      
+      }
       if(jsonData[i].quaas===true){
-      quaas.push (div);
-    }
-      div.classList.add("black");
-      parentSect.append(div)
+        div.addEventListener("click", ()=>{
+          quaa.play();
+        })
+        //only ones with kuks show
+        div.classList.add("black");
+        parentSect.append(div)      
+      }
+      if(jsonData[i].moans===true){
+        div.addEventListener("click", ()=>{
+          moan.play();
+        })
+        //only ones with kuks show
+        div.classList.add("black");
+        parentSect.append(div)      
+      }
     };
   }
-    squirrelDiv.innerHTML = elements;
-    squirrelData.appendChild(squirredDiv);
-  
+    // squirrelDiv.innerHTML = elements;
+    // squirrelData.appendChild(squirrelDiv);
 }
-// for(let i=0; i<quaas.length; i++){
+
+
+
+// for(let i=0; i<kuks.length; i++){
       
 
-// quaas[i].addEventListener("mouseover",(event)=>{
-//   quaa.play();
-//       console.log(quaas)
+// kuks[i].addEventListener("mouseover",(event)=>{
+//   kuk.play();
+//       console.log(kuks)
 //     )};
 //   };
+
 
 logJSONData();
 
